3f005c14c881e9096eeb56f0e1af7b123dfde89d
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-07-18 17:14:54 +0300

    вніс виправлення  версія 1,4

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -7,9 +7,12 @@
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("is-open");
+    
     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
+   
+    menuBtnRef.classList.toggle("is-open");
     mobileMenuRef.classList.toggle("is-open");
+    
+    
   });
 })();
